80dbc74a3850135dfb9027d772748526b602a549
Michal Kokocinski <michal@waat.eu>
2020-02-29 19:17:23 +0100

    Add Folder name and description to KML export.

--- src/utils/exportToKML.js
+++ src/utils/exportToKML.js
@@ -6,10 +6,18 @@ import texts from './texts'
 const exportToKML = locations => {
   // Currently only polish language is supported.
   try {
+    const today = new Date()
+    const dd = String(today.getDate()).padStart(2, '0')
+    const mm = String(today.getMonth() + 1).padStart(2, '0')
+    const yyyy = today.getFullYear()
+    const date = `${yyyy}-${mm}-${dd}`
+
     const header = [
       '<?xml version="1.0" encoding="UTF-8"?>',
       '<kml xmlns="http://www.opengis.net/kml/2.2">',
       '  <Folder>',
+      '    <name>Wiating</name>',
+      `    <description>Eksport z aplikacji Wiating z dnia ${date}.</description>`,
     ]
     const footer = [
       '  </Folder>',
@@ -51,14 +59,9 @@ const exportToKML = locations => {
       ? dataURI
       : URL.createObjectURL(blob)
 
-    const today = new Date()
-    const dd = String(today.getDate()).padStart(2, '0')
-    const mm = String(today.getMonth() + 1).padStart(2, '0')
-    const yyyy = today.getFullYear()
-
     let downloadLink = document.createElement('a')
     downloadLink.href = buildedURI
-    downloadLink.download = `wiating-${yyyy}-${mm}-${dd}.kml`
+    downloadLink.download = `wiating-${date}.kml`
     document.body.appendChild(downloadLink)
     downloadLink.click()
     document.body.removeChild(downloadLink)
