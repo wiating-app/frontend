e7006933562de0dc2f4c078744bf4ca4b300848f
Michal Kokocinski <michal@waat.eu>
2020-03-05 16:38:27 +0100

    Remove authorization from api declaration.

--- src/api.js
+++ src/api.js
@@ -3,13 +3,6 @@ import axios from 'axios'
 export const CancelToken = axios.CancelToken
 export const isCancel = axios.isCancel
 
-const authorization = () => {
-  const user = localStorage.getItem('currentUser')
-  return user
-    ? { 'Authorization': `Bearer ${JSON.parse(user).token}` }
-    : {}
-}
-
 const api = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}/wiating/`,
   timeout: 10000,
@@ -18,7 +11,6 @@ const api = axios.create({
     'Accept': 'application/json',
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json; charset=utf-8',
-    ...authorization(),
   },
 })
 
