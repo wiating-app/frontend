dfebf80ca7bc6849692746e40807dfc2924b3297
Michal Kokocinski <michal@waat.eu>
2020-03-09 18:28:04 +0100

    Clickable field hints.

--- src/components/HintWrapper.js
+++ src/components/HintWrapper.js
@@ -1,24 +1,39 @@
 import React from 'react'
-import { Tooltip, Grid } from '@material-ui/core'
+import { Tooltip, Grid, ClickAwayListener, IconButton } from '@material-ui/core'
 import { Info } from '@material-ui/icons'
 import { makeStyles } from '@material-ui/core/styles'
 
 
 const HintWrapper = ({ message, children }) => {
   const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
 
   return (
     <Grid container justify='space-between'>
       <Grid item xs={11}>
         {children}
       </Grid>
-      <Grid item xs>
-        <Tooltip
-          title={<div className={classes.message}>{message}</div>}
-          className={classes.root}
-        >
-          <Info className={classes.icon} />
-        </Tooltip>
+      <Grid item xs={1}>
+        <ClickAwayListener onClickAway={() => setOpen(false)}>
+          <div>
+            <Tooltip
+              PopperProps={{
+                disablePortal: true,
+              }}
+              onClose={() => setOpen(false)}
+              open={open}
+              disableFocusListener
+              disableHoverListener
+              disableTouchListener
+              title={<div className={classes.message}>{message}</div>}
+              className={classes.tooltip}
+            >
+              <IconButton onClick={() => setOpen(true)}>
+                <Info />
+              </IconButton>
+            </Tooltip>
+          </div>
+        </ClickAwayListener>
       </Grid>
     </Grid>
   )
@@ -26,10 +41,7 @@ const HintWrapper = ({ message, children }) => {
 
 const useStyles = makeStyles(theme => ({
   root: {
-    marginTop: 20,
-  },
-  icon: {
-    cursor: 'help',
+    tooltip: 20,
   },
   message: {
     fontSize: theme.typography.fontSize,
