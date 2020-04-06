911555b981a9f5303db1699f22e9104892cc60ef
Michal Kokocinski <michal@waat.eu>
2020-04-06 21:45:18 +0200

    Add map offset to show markers that are on the edge of a screen.

--- src/components/Map.js
+++ src/components/Map.js
@@ -87,182 +87,188 @@ const Map = React.forwardRef(({
   }
 
   return (
-    <MapComponent
-      ref={mapRef}
-      className={classes.root}
+    // Use wrapper to set offset to load markers that are on the edge of a screen.
+    <div
+      className={classes.offsetWrapper}
       style={isLocationTabOpen && isMobile
         ? isPhone
           ? { height: theme.layout.mobileMiniMapHeight }
           : { marginLeft: theme.layout.locationTabWidth }
         : {}
       }
-      center={center}
-      zoom={zoom}
-      minZoom={5}
-      maxZoom={18}
-      maxBounds={[[-90, -180], [90, 180]]}
-      zoomControl={false}
-      onMoveEnd={() => handleLoadMapMarkers()}
-      onContextMenu={e => {
-        if (!editMode) {
-          if (isLoggedIn) {
-            setContextMenu(!contextMenu)
-            setActiveMarker(contextMenu ? null : e.latlng)
-          }
-          closeTab()
-        }
-      }}
-      onClick={e => {
-        if (contextMenu) {
-          // If context menu is opened, close it.
-          setContextMenu(false)
-          setActiveMarker(false)
-        } else if (editMode && isLoggedIn && !activeMarker) {
-          // If location creation form has beem opened from URL and there are no
-          // coordinates given yet, set the coordinates and active marker.
-          openAddMarkerTab(e.latlng)
-          setActiveMarker(e.latlng)
-          updateCoordinates(e.latlng)
-        } else if (isLocationTabOpen && !editMode) {
-          // Dismiss the location details drawer, when clicking on a map.
-          closeTab()
-          setContextMenu(false)
-          setActiveMarker(false)
-        }
-      }}
     >
-      <TileLayer
-        url='https://mapserver.mapy.cz/turist-m/{z}-{x}-{y}'
-        attribution={`&copy; <a href="https://www.seznam.cz" target="_blank" rel="noopener">Seznam.cz, a.s.</a>, &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener">OpenStreetMap</a>, &copy; NASA`}
-      />
-      <MarkerClusterGroup
-        showCoverageOnHover={false}
-        maxClusterRadius={60}
-        disableClusteringAtZoom={11}
-        spiderfyOnMaxZoom={false}
-        iconCreateFunction={cluster => {
-          const count = cluster.getChildCount()
-          return new DivIcon({
-            html: count,
-            className: classes.woodboardCluster,
-            iconSize: [40, 40],
-          })
+      <MapComponent
+        ref={mapRef}
+        className={classes.mapOffset}
+        center={center}
+        zoom={zoom}
+        minZoom={5}
+        maxZoom={18}
+        maxBounds={[[-90, -180], [90, 180]]}
+        zoomControl={false}
+        onMoveEnd={() => handleLoadMapMarkers()}
+        onContextMenu={e => {
+          if (!editMode) {
+            if (isLoggedIn) {
+              setContextMenu(!contextMenu)
+              setActiveMarker(contextMenu ? null : e.latlng)
+            }
+            closeTab()
+          }
+        }}
+        onClick={e => {
+          if (contextMenu) {
+            // If context menu is opened, close it.
+            setContextMenu(false)
+            setActiveMarker(false)
+          } else if (editMode && isLoggedIn && !activeMarker) {
+            // If location creation form has beem opened from URL and there are no
+            // coordinates given yet, set the coordinates and active marker.
+            openAddMarkerTab(e.latlng)
+            setActiveMarker(e.latlng)
+            updateCoordinates(e.latlng)
+          } else if (isLocationTabOpen && !editMode) {
+            // Dismiss the location details drawer, when clicking on a map.
+            closeTab()
+            setContextMenu(false)
+            setActiveMarker(false)
+          }
         }}
       >
-        {points && points.map(item => {
-          const { location: { lat, lon }, type } = item
-
-          return <Marker
-            key={item.id}
-            icon={new Icon({
-              iconUrl: getIconUrl(type),
-              iconSize: [30, 30],
-              iconAnchor: [15, 30],
-            })}
-            position={[lat, lon]}
-            onClick={() => {
-              openLocationTab(item)
-              setContextMenu(null)
-              setActiveMarker([lat, lon])
-            }}
-            opacity={editMode ? 0.5 : 1}
-          />
-        })}
-      </MarkerClusterGroup>
-      {activeMarker &&
-        <Marker
-          icon={new Icon({
-            iconUrl: '/location-icons/point.svg',
-            iconSize: [40, 40],
-            iconAnchor: [20, 40],
-          })}
-          zIndexOffset={1100}
-          position={activeMarker}
-          draggable={editMode}
-          onMoveEnd={e => {
-            if (editMode) {
-              updateCoordinates(e.target.getLatLng())
-            }
-          }}
+        <TileLayer
+          url='https://mapserver.mapy.cz/turist-m/{z}-{x}-{y}'
+          attribution={`&copy; <a href="https://www.seznam.cz" target="_blank" rel="noopener">Seznam.cz, a.s.</a>, &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener">OpenStreetMap</a>, &copy; NASA`}
         />
-      }
-      {activeMarker && contextMenu &&
-        <Popup
-          position={activeMarker}
-          closeButton={false}
-          className={classes.popup}
+        <MarkerClusterGroup
+          showCoverageOnHover={false}
+          maxClusterRadius={60}
+          disableClusteringAtZoom={11}
+          spiderfyOnMaxZoom={false}
+          iconCreateFunction={cluster => {
+            const count = cluster.getChildCount()
+            return new DivIcon({
+              html: count,
+              className: classes.woodboardCluster,
+              iconSize: [40, 40],
+            })
+          }}
         >
-          <ContextMenu addMarker={() => {
-            setContextMenu(null)
-            openAddMarkerTab(activeMarker)
-            mapRef.current.leafletElement.setView(activeMarker)
-          }} />
-        </Popup>
-      }
-      {currentLocation &&
-        <>
-          {locationAccuracy && locationAccuracy > 30 &&
-            <Circle
-              center={currentLocation}
-              radius={locationAccuracy}
+          {points && points.map(item => {
+            const { location: { lat, lon }, type } = item
+
+            return <Marker
+              key={item.id}
+              icon={new Icon({
+                iconUrl: getIconUrl(type),
+                iconSize: [30, 30],
+                iconAnchor: [15, 30],
+              })}
+              position={[lat, lon]}
+              onClick={() => {
+                openLocationTab(item)
+                setContextMenu(null)
+                setActiveMarker([lat, lon])
+              }}
+              opacity={editMode ? 0.5 : 1}
             />
-          }
+          })}
+        </MarkerClusterGroup>
+        {activeMarker &&
           <Marker
             icon={new Icon({
-              iconUrl: '/location-icons/current.svg',
-              iconSize: [24, 24],
-              iconAnchor: [12, 12],
+              iconUrl: '/location-icons/point.svg',
+              iconSize: [40, 40],
+              iconAnchor: [20, 40],
             })}
-            zIndexOffset={1000}
-            position={currentLocation}
+            zIndexOffset={1100}
+            position={activeMarker}
+            draggable={editMode}
+            onMoveEnd={e => {
+              if (editMode) {
+                updateCoordinates(e.target.getLatLng())
+              }
+            }}
           />
-        </>
-      }
-      {(!isLocationTabOpen || !isPhone) &&
-        <>
-          <ZoomControl position='topright' />
-          <Control position='topright' className='leaflet-bar'>
-            <a
-              className={classes.customControl}
-              onClick={() => currentLocation &&
-                mapRef.current.leafletElement.flyTo(currentLocation, 14)
-              }
-              disabled={!currentLocation}
-            >
-              {currentLocation
-                ? <GpsFixed className={classes.customControlIcon} />
-                : <GpsNotFixed className={classes.customControlIcon} />
-              }
-            </a>
-          </Control>
-          {!editMode &&
-            <Control position='topright' className='leaflet-bar'>
-              <Tooltip title='Eksportuj aktualny widok do KML' placement='left'>
-                <a
-                  className={classes.customControl}
-                  onClick={() => exportToKML(points)}
-                  disabled={!points || !points.length}
-                >KML</a>
-              </Tooltip>
-            </Control>
-          }
-        </>
-      }
-      <Control position='bottomright'>
-        {currentLocation &&
-          <Typography
-            variant='caption'
-            className={classes.currentLocation}
-          >Dokładność GPS: {Math.round(locationAccuracy)} m</Typography>
         }
-      </Control>
-      <ScaleControl position='bottomright' imperial={false} />
-      {!isMobile && !editMode &&
-        <Control position='topleft'>
-          <Legend boxed />
+        {activeMarker && contextMenu &&
+          <Popup
+            position={activeMarker}
+            closeButton={false}
+            className={classes.popup}
+          >
+            <ContextMenu addMarker={() => {
+              setContextMenu(null)
+              openAddMarkerTab(activeMarker)
+              mapRef.current.leafletElement.setView(activeMarker)
+            }} />
+          </Popup>
+        }
+        {currentLocation &&
+          <>
+            {locationAccuracy && locationAccuracy > 30 &&
+              <Circle
+                center={currentLocation}
+                radius={locationAccuracy}
+              />
+            }
+            <Marker
+              icon={new Icon({
+                iconUrl: '/location-icons/current.svg',
+                iconSize: [24, 24],
+                iconAnchor: [12, 12],
+              })}
+              zIndexOffset={1000}
+              position={currentLocation}
+            />
+          </>
+        }
+        {(!isLocationTabOpen || !isPhone) &&
+          <>
+            <ZoomControl position='topright' />
+            <Control position='topright' className='leaflet-bar'>
+              <a
+                className={classes.customControl}
+                onClick={() => currentLocation &&
+                  mapRef.current.leafletElement.flyTo(currentLocation, 14)
+                }
+                disabled={!currentLocation}
+              >
+                {currentLocation
+                  ? <GpsFixed className={classes.customControlIcon} />
+                  : <GpsNotFixed className={classes.customControlIcon} />
+                }
+              </a>
+            </Control>
+            {!editMode &&
+              <Control position='topright' className='leaflet-bar'>
+                <Tooltip title='Eksportuj aktualny widok do KML' placement='left'>
+                  <a
+                    className={classes.customControl}
+                    onClick={() => exportToKML(points)}
+                    disabled={!points || !points.length}
+                  >KML</a>
+                </Tooltip>
+              </Control>
+            }
+          </>
+        }
+        <Control position='bottomright'>
+          {currentLocation && (!isLocationTabOpen || !isPhone) &&
+            <Typography
+              component='div'
+              variant='caption'
+              className={classes.currentLocation}
+            >Dokładność GPS: {Math.round(locationAccuracy)} m</Typography>
+          }
         </Control>
-      }
-    </MapComponent>
+        <ScaleControl position='bottomright' imperial={false} />
+        {!isMobile && !editMode &&
+          <Control position='topleft'>
+            <Legend boxed />
+          </Control>
+        }
+      </MapComponent>
+    </div>
   )
 })
 
@@ -271,8 +277,28 @@ Map.defaultProps = {
 }
 
 const useStyles = makeStyles(theme => ({
-  root: {
+  offsetWrapper: {
     flexGrow: 1,
+    position: 'relative',
+  },
+  // Map offset to load markers that are on the edge of a screen.
+  mapOffset: {
+    position: 'absolute',
+    top: 0,
+    left: theme.spacing(-2),
+    bottom: theme.spacing(-4),
+    right: theme.spacing(-2),
+    // Offset must be compensed on controls.
+    '& .leaflet-right': {
+      right: theme.spacing(2),
+    },
+    '& .leaflet-bottom': {
+      bottom: theme.spacing(4),
+    },
+    '& .leaflet-left': {
+      left: theme.spacing(2),
+    },
+    // Add light shadow to all markers.
     '& .leaflet-marker-icon': {
       filter: 'drop-shadow(0 0 1px rgb(0,0,0))',
     },
@@ -316,8 +342,9 @@ const useStyles = makeStyles(theme => ({
     fontSize: 18,
   },
   currentLocation: {
-    backgroundColor: 'rgba(255,255,255,0.85)',
-    padding: 2,
+    backgroundColor: 'rgba(255,255,255,0.75)',
+    padding: '0 2px',
+    fontSize: 11,
   },
 }))
 
