50acdf62bf97ad1141d1dc596bca7958a043f3ce
Michal Kokocinski <michal@waat.eu>
2020-08-26 21:12:04 +0200

    Content of Polityka prywatności.

--- src/components/PolitykaPrywatnosci.js
+++ src/components/PolitykaPrywatnosci.js
@@ -3,8 +3,100 @@ import Modal from './Modal'
 
 const PolitykaPrywatnosci = () =>
   <Modal>
+
     <h2>Polityka prywatności</h2>
-    <p>Dział w budowie</p>
+
+    <h3>§1 Postanowienia ogólne</h3>
+
+    <ol>
+      <li>Niniejszy dokument określa Politykę Prywatności aplikacji internetowej Wiating, która w szczególności obejmuje regulacje dotyczące ochrony danych osobowych oraz bezpieczeństwa innych danych wprowadzonych do Serwisu przez Użytkownika.</li>
+      <li>Polityka Prywatności stanowi integralny załącznik do Regulaminu.</li>
+    </ol>
+
+    <h3>§2. Definicje</h3>
+
+    <p>Terminy użyte w niniejszym dokumencie oznaczają:</p>
+    <ol>
+      <li>Administrator – Administracja Grupy Wiating - Dariusz Hajduk, adres e-mail: <a href='mailto:wiating@wiating.eu'>wiating@wiating.eu</a>;</li>
+      <li>Serwis – aplikacja internetowa pod adresem wiating.eu oraz wszelkie jej podstrony;</li>
+      <li>Strony – Administrator i Użytkownik;</li>
+      <li>Użytkownik – osoba fizyczna, która korzysta z Serwisu i podaje w jego ramach swoje dane osobowe.</li>
+    </ol>
+
+    <h3>§3. Ochrona danych osobowych</h3>
+
+    <ol>
+      <li>Administrator jest Administratorem danych osobowych w rozumieniu rozporządzenia o ochronie danych osobowych z dnia 27 kwietnia 2016 r. (RODO).</li>
+      <li>Administrator przetwarza dane w zakresie, czasie i celach każdorazowo wskazanych w treściach udostępnionych pod formularzami służącymi do zbierania danych osobowych od Użytkownika.</li>
+      <li>Dane osobowe będą przekazywane wyłącznie zaufanym podwykonawcom Administratora, tj. dostawcom usług informatycznych, firmie księgowej, administracji.</li>
+    </ol>
+
+    <h3>§4. Uprawnienia Użytkownika</h3>
+
+    <ol>
+      <li>W przypadku zmiany danych osobowych, Użytkownik powinien uaktualnić je wysyłając stosowną wiadomość do Administratora.</li>
+      <li>Użytkownikowi przysługuje prawo żądania dostępu do treści swoich danych osobowych, ich sprostowania, usunięcia oraz prawo do ograniczenia ich przetwarzania. Ponadto także prawo do cofnięcia zgody w dowolnym momencie bez wpływu na zgodność z prawem przetwarzania, prawo do przenoszenia danych oraz prawo do wniesienia sprzeciwu wobec przetwarzania danych osobowych.Użytkownikowi przysługuje prawo wniesienia skargi do Prezesa Urzędu Ochrony Danych Osobowych.</li>
+      <li>Podanie danych osobowych jest dobrowolne, jednakże brak wyrażenia zgody na przetwarzanie danych uniemożliwia korzystanie z Serwisu.</li>
+      <li>Administrator może odmówić usunięcia danych osobowych Użytkownika, jeżeli zachowanie danych osobowych jest konieczne ze względu na obowiązek nałożony na Administratora przez przepisy prawa.</li>
+    </ol>
+
+    <h3>§5. Techniczna ochrona danych</h3>
+
+    <ol>
+      <li>Administrator wykorzystuje wszelkie techniczne i organizacyjne sposoby, by zapewnić bezpieczeństwo danym osobowym Użytkownika i chronić je przed przypadkowym lub celowym zniszczeniem, przypadkową utratą, modyfikacją, nieautoryzowanym ujawnieniem lub dostępem. Informacje są przechowywane i przetwarzane na serwerach o wysokim stopniu zabezpieczeń, z zachowaniem odpowiednich środków bezpieczeństwa, spełniających wymogi polskiego prawa.</li>
+      <li>Administrator zobowiązuje się do przechowywania kopii bezpieczeństwa zawierających dane osobowe Użytkownika.</li>
+      <li>Powierzone dane są składowane na najwyższej klasy sprzęcie i serwerach w odpowiednio zabezpieczonych centrach przechowywania informacji, do których dostęp mają tylko upoważnione osoby.</li>
+      <li>Administrator przeprowadza czynności związane z przetwarzaniem danych osobowych z poszanowaniem wszystkich wymogów prawnych i technicznych nałożonych na niego przez przepisy dotyczące ochrony danych osobowych.</li>
+    </ol>
+
+    <h3>§6. Polityka cookies</h3>
+
+    <ol>
+      <li>Dla wygody Użytkowników Serwis używa plików cookies m.in. w celu dostosowania serwisu do potrzeb użytkowników oraz w celach statystycznych. Cookies to niewielkie pliki tekstowe wysyłane przez serwis internetowy, który odwiedza internauta, do urządzenia internauty.</li>
+      <li>W ramach Serwisu stosowane są pliki cookies oraz local storage. Są to przechowywane w urządzeniu końcowym Użytkownika niewielkie informacje tekstowe.</li>
+      <li>
+        W Serwisie wykorzystujemy następujące rodzaje plików Cookies i local storage:
+        <ol>
+          <li><em>niezbędne</em> - umożliwiają korzystanie z usług dostępnych w ramach Serwisu, np. wykorzystywane przy obsłudze autoryzacji użytkowników;</li>
+          <li><em>zabezpieczające</em> - służą do zapewnienia bezpieczeństwa, np. wykorzystywane do wykrywania nadużyć w zakresie korzystania z usług Serwisu;</li>
+          <li><em>funkcjonalne</em> - umożliwiają zapamiętanie wybranych przez Użytkownika ustawień i personalizację interfejsu Użytkownika, np. w zakresie wybranego języka, ostatniej lokalizacji, wyglądu Serwisu itp.;</li>
+          <li><em>integracyjne</em> - związane są z wykorzystywanymi w Serwisie usługami firm trzecich, np. Google Analytics.</li>
+        </ol>
+      </li>
+      <li>Serwisy obce, których materiały przedstawiamy, mogą także używać cookies, które umożliwiają logowanie się.</li>
+      <li>W przeglądarce internetowej można zmienić ustawienia dotyczące cookies. Brak zmiany tych ustawień oznacza akceptację dla stosowanych tu cookies.</li>
+      <li>Zmiana konfiguracji przeglądarki internetowej, która uniemożliwia lub ogranicza przechowywanie plików cookies i local storage na urządzeniu końcowym Użytkownika może spowodować ograniczenia funkcjonalności świadczonych usług. Do podobnych skutków może prowadzić wykasowanie plików cookies i local storage w trakcie świadczenia usługi.</li>
+    </ol>
+
+    <h3>§7. Logi</h3>
+
+    <ol>
+      <li>
+        Zgodnie z praktyką większości serwisów www przechowujemy zapytania HTTP kierowane do naszego serwera (logi serwera). W związku z powyższym przechowujemy:
+        <ol>
+          <li>adresy IP, z których użytkownicy przeglądają treści informacyjne naszego serwisu;</li>
+          <li>czas nadejścia zapytania,</li>
+          <li>czas wysłania odpowiedzi,</li>
+          <li>nazwę stacji klienta - identyfikacja realizowana przez protokół HTTP,</li>
+          <li>informacje o błędach jakie nastąpiły przy realizacji transakcji HTTP,</li>
+          <li>adres URL strony po</li>
+          <li>przednio odwiedzanej przez użytkownika (referer link).</li>
+        </ol>
+      </li>
+      <li>Dane gromadzone w dziennikach logów, dane są wykorzystywane tylko i wyłącznie do celów administrowania Serwisem.</li>
+      <li>Zebrane logi przechowywane są przez czas nieokreślony jako materiał pomocniczy służący do administrowania Serwisem. Informacje w nich zawarte nie są ujawniane nikomu poza osobami upoważnionymi do administrowania Serwisem. Na podstawie plików logów mogą być generowane statystyki stanowiące pomoc w administrowaniu. Zbiorcze podsumowania w postaci takich statystyk nie zawierają żadnych cech identyfikujących osoby odwiedzające serwis.</li>
+      <li></li>
+      <li></li>
+    </ol>
+
+    <h3>§8. Kontakt</h3>
+
+    <ol>
+      <li>Użytkownik może w dowolnym czasie skontaktować się z Administratorem w celu uzyskania informacji o tym, czy i w jaki sposób Administrator wykorzystuje jego dane osobowe.</li>
+      <li>Użytkownik może również zwrócić się do Administratora z prośbą o usunięcie jego danych osobowych w całości lub w określonej części.</li>
+      <li>Z Administratorem można skontaktować się wysyłając wiadomość na adres e-mail: <a href='mailto:wiating@wiating.eu'>wiating@wiating.eu</a>.</li>
+    </ol>
+
   </Modal>
 
 export default PolitykaPrywatnosci
