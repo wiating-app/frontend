497710456eb6bd26afce4d4e4557f2e1834df1dc
Michal Kokocinski <michal@waat.eu>
2020-09-16 11:54:04 +0200

    Add info about affected fields on log listing.

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
+import { Typography, Box } from '@material-ui/core'
 import Modal from './Modal'
 import Table from './Table'
 import Pagination from './Pagination'
@@ -36,8 +36,15 @@ const Logs = ({
             <Table
               data={logs.map(item => ({
                 timestamp: item._source.timestamp,
-                location: <OpenInNewCard path={`/location/${item._source.doc_id}`}>{item._source.name}</OpenInNewCard>,
-                user: item._source.modified_by === user.sub ? 'Ja' : item._source.modified_by,
+                location: <>
+                  <OpenInNewCard path={`/location/${item._source.doc_id}`}>{item._source.name}</OpenInNewCard>
+                  <Typography variant='caption' component='div'>
+                    {Object.keys(item._source.changes).join(', ')}
+                  </Typography>
+                </>,
+                user: <Box whiteSpace='nowrap'>{
+                  item._source.modified_by === user.sub ? 'Ja' : item._source.modified_by}
+                </Box>,
                 actions: <Actions
                   primary={[
                     { label: 'Szczegóły', action: () => setDetails({ id: item._id, ...item._source }) },
@@ -46,8 +53,8 @@ const Logs = ({
               }))}
               labels={[
                 { name: 'Data', field: 'timestamp' },
-                { name: 'Lokacja', field: 'location' },
-                { name: 'Użytkownik', field: 'user' },
+                { name: 'Lokacja i zmienione pola', field: 'location' },
+                { name: 'Autor zmiany', field: 'user' },
                 { name: '', field: 'actions' },
               ]}
             />
