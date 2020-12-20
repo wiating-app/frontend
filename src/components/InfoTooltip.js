a9fd31ce5c603e9ad358541bcf92067e27a5484d
Michal Kokocinski <michal@waat.eu>
2020-12-20 13:14:31 +0100

    Current site version indicator.

---
+++ src/components/InfoTooltip.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import {
+  Tooltip,
+  ClickAwayListener,
+  IconButton,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { InfoOutlined } from '@material-ui/icons'
+
+
+const InfoTooltip = ({
+  placement,
+  large,
+  icon,
+  children,
+  className,
+}) => {
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
+
+  const TriggerComponent = large ? IconButton : 'span'
+  const IconComponent = icon || InfoOutlined
+
+  return (
+    <ClickAwayListener onClickAway={() => setOpen(false)}>
+      <Tooltip
+        placement={placement}
+        onClose={() => setOpen(false)}
+        open={open}
+        disableFocusListener
+        disableHoverListener
+        disableTouchListener
+        title={<div className={classes.message}>{children}</div>}
+        className={classes.tooltip}
+      >
+        <TriggerComponent
+          onClick={() => setOpen(true)}
+          onMouseOver={() => setOpen(true)}
+          onMouseOut={() => setOpen(false)}
+          className={large ? '' : classes.smallTrigger}
+        >
+          <IconComponent className={`${classes.icon} ${className || ''}`} />
+        </TriggerComponent>
+      </Tooltip>
+    </ClickAwayListener>
+  )
+}
+
+Tooltip.defaultProps = {
+  placement: 'bottom',
+}
+
+const useStyles = makeStyles(theme => ({
+  message: {
+    fontSize: theme.typography.fontSize,
+    lineHeight: 'normal',
+    fontWeight: theme.typography.fontWeightRegular,
+    pointerEvents: 'none',
+  },
+  icon: {
+    pointerEvents: 'none',
+  },
+  smallTrigger: {
+    display: 'inline-block',
+    verticalAlign: 'middle',
+    '& > *': {
+      fontSize: '1.33em',
+    },
+  },
+}))
+
+export default InfoTooltip
