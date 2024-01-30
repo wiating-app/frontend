aec5bdb993f5882df66d386aa84038caf5d9aea1
Michal Kokocinski <michal@kokocinski.me>
2024-01-30 14:33:00 +0100

    Use OSM tile server.

--- src/components/Map.js
+++ src/components/Map.js
@@ -153,8 +153,8 @@ const Map = ({
           handleLoadMapMarkers={handleLoadMapMarkers}
         />
         <TileLayer
-          url='https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
-          attribution={`Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)`}
+          url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+          attribution={`&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`}
         />
         <PixiOverlay
           markers={markers.map(item => {
