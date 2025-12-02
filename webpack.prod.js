642d0f9f66759cf23270b494a4fa5924ae6bd523
Michal Kokocinski <michal@kokocinski.me>
2025-12-02 19:00:52 +0100

    Fix service worker path

--- webpack.prod.js
+++ webpack.prod.js
@@ -20,7 +20,7 @@ module.exports = merge(common, {
 
     new InjectManifest({
       swSrc: './src/serviceWorker.ts',
-      swDest: 'service-worker.ts',
+      swDest: 'service-worker.js',
       exclude: [
         /_redirects$/,
       ],
